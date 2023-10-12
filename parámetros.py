c7284efb9e06c427fa967db39eac688c70b02ca7
nina <casanova.gianina@gmail.com>
2023-10-12 07:30:06 -0600

    nuevo

---
+++ parámetros.py
@@ -0,0 +1,11 @@
+# parámetros por valor o por referencia
+def funcio(x):
+    # funcion
+    print("Origina: ",x)
+    x = 10
+    print("Nuevo: ",x)
+
+    # código principal
+    x = 20
+    y = function(x)
+    print("Valor retornado: ",x)
